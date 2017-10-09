a47c4f894ec3ae376e70c5320c09340fc426dc6b
Jani Kraner <jani.kraner@gmail.com>
2017-10-09 11:57:08 +0100

    Enable Middleman's minify css & js

--- config.rb
+++ config.rb
@@ -42,7 +42,7 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
