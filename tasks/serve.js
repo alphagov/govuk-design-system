fe7836de6c1b8c7708e189525cc6c5ffdbd5d00e
Tim Paul <contact@timpaul.co.uk>
2017-12-22 14:46:38 +0000

    Add proof-of-concept backlog

--- tasks/serve.js
+++ tasks/serve.js
@@ -1,15 +1,59 @@
+const fs = require('fs')
+
 const browsersync = require('metalsmith-browser-sync') // setup synchronised browser testing
 const metalsmith = require('../lib/metalsmith') // configured static site generator
 
 const paths = require('../config/paths.json') // specify paths to main working directories
 
-// setup synchronised browser testing
-metalsmith.use(browsersync({
-  server: paths.public, // server directory
-  files: [paths.source + '**/*', paths.views + '**/*'] // files to watch
-}))
-
-// build to destination directory
-metalsmith.build(function (err, files) {
-  if (err) { throw err }
+// query GitHub to get backlog
+const GithubGraphQLApi = require('node-github-graphql')
+const github = new GithubGraphQLApi({
+  token: process.env.GITHUB_API_TOKEN_DS
 })
+
+// get backlog data
+
+github.query(`
+query {
+  repository(owner:"alphagov", name:"govuk-design-system-backlog") {
+    project(number:3){
+      name,
+      columns(first:4){
+        nodes{
+          name,
+          cards(first:100){
+            edges{
+              node{
+                note,
+                content{
+					... on Issue {
+						title,
+                    	url
+    				}
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`, null, (res, err) => {
+ 	fs.writeFileSync('data/backlog.json', JSON.stringify(res, null, 2))
+  	
+  	// setup synchronised browser testing
+	metalsmith.use(browsersync({
+	  server: paths.public, // server directory
+	  files: [paths.source + '**/*', paths.views + '**/*'] // files to watch
+	}))
+
+	// build to destination directory
+	metalsmith.build(function (err, files) {
+	  if (err) { throw err }
+	})
+
+})
+
+
+
