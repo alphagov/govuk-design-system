f01759293ecda68e1016a464ad4a21276a9f99d7
alex-ju <alex-ju@users.noreply.github.com>
2018-01-10 13:22:52 +0000

    Refactor tabs.js to use 'js-' prefixes for DOM manipulation

--- src/javascripts/components/tabs.js
+++ src/javascripts/components/tabs.js
@@ -7,17 +7,29 @@
 
     // Reset tabs and containers
     resetTabs: function (id) {
+      // Check if we have an id
+      if (!id) {
+        console.error('id is undefined')
+        return
+      }
+
       var $example = $(id).parent()
 
       // Reset state
-      $example.find('.app-c-tabs__item').removeClass('app-c-tabs__item--current')
-      $example.find('.app-c-tabs__heading').removeClass('app-c-tabs__heading--current')
-      $example.find('.app-c-tabs__item a').removeAttr('aria-selected')
-      $example.find('.app-c-tabs__container').hide().attr('aria-hidden', 'true')
+      $example.find('.js-tabs__item').removeClass('app-c-tabs__item--current')
+      $example.find('.js-tabs__heading').removeClass('app-c-tabs__heading--current')
+      $example.find('.js-tabs__item a').removeAttr('aria-selected')
+      $example.find('.js-tabs__container').hide().attr('aria-hidden', 'true')
     },
 
     // Activate current tab
     activateCurrentTab: function (id) {
+      // Check if we have an id
+      if (!id) {
+        console.error('id is undefined')
+        return
+      }
+
       // Reset tabs
       GOVUK.tabs.resetTabs(id)
 
@@ -58,31 +70,31 @@
       GOVUK.tabs.resetTabs('#' + id)
     },
 
-    init: function () {
+    init: function (selector) {
       // If more than one container
-      var $examples = $('.app-c-example-wrapper')
+      var $examples = $(selector)
       $.each($examples, function (key, obj) {
-        if ($(obj).find('.app-c-tabs__container').length > 1) {
+        if ($(obj).find('.js-tabs__container').length > 1) {
           // Hide all containers
-          $(obj).find('.app-c-tabs__container').hide()
+          $(obj).find('.js-tabs__container').hide()
 
           // Add close button to each container
-          $(obj).find('.app-c-tabs__container').append('<a class="app-c-link--close app-o-chevron--top" href="#close">Close</a>')
-          $(obj).find('.app-c-tabs__container').addClass('app-c-tabs__container--with-close-button')
+          $(obj).find('.js-tabs__container').append('<a class="app-c-link--close js-link--close app-o-chevron--top" href="#close">Close</a>')
+          $(obj).find('.js-tabs__container').addClass('app-c-tabs__container--with-close-button')
         }
       })
 
       // Bind tab item click
-      $('.app-c-tabs__item a').click(GOVUK.tabs.clickTabItem)
+      $('.js-tabs__item a').click(GOVUK.tabs.clickTabItem)
 
       // Bind accordion item click
-      $('.app-c-tabs__heading a').click(GOVUK.tabs.clickTabItem)
+      $('.js-tabs__heading a').click(GOVUK.tabs.clickTabItem)
 
       // Bind close container link
-      $('.app-c-link--close').click(GOVUK.tabs.clickCloseContainer)
+      $('.js-link--close').click(GOVUK.tabs.clickCloseContainer)
 
       // Show open containers
-      $('.app-c-tabs__heading a[open]').click()
+      $('.js-tabs__heading--open a').click()
     }
   }
 
