a0264ad58689eb508b1d8f3075286b2a57a776b5
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2018-02-02 10:14:11 +0000

    Auto-load GOV.UK Frontend macros
    Glob the file system for macro file within the @govuk-frontend node modules folder, parse each file and grab any macros they export, and then make them available as globals.
    
    This allows us to remove the `{% from “component/macro.njk" import govukComponent %}` lines from the examples we create, which means that Prototype Kit users will not have to worry about what to do with that line.
    
    Because we need a Nunjucks environment we can use to parse the templates, the config for the nunjucks engine has been moved into the config folder and required in both places.
    
    We should be able to adopt this in the Prototype Kit as well (though we’ll probably need to swap the spread operators out for something that works in older versions of Node as well).

---
+++ config/nunjucks.js
@@ -0,0 +1,8 @@
+// This is a Javascript file because you can't comment JSON :(
+
+module.exports = {
+  autoescape: true, // escape all output by default
+  noCache: true, // never use a cache and recompile templates each time
+  trimBlocks: true, // automatically remove trailing newlines from a block/tag
+  lstripBlocks: true, // automatically remove leading whitespace from a block/tag
+}
