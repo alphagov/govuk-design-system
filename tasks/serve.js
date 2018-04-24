d6ccf3edc08761905bb5553eae1104ce568bb278
Matt Hobbs <matt@nooshu.com>
2018-04-24 09:21:13 +0100

    Added service worker with precaching using Workbox 3.1.0 for offline documentation

--- tasks/serve.js
+++ tasks/serve.js
@@ -1,11 +1,13 @@
 const browsersync = require('metalsmith-browser-sync') // setup synchronised browser testing
 const metalsmith = require('../lib/metalsmith') // configured static site generator
-
 const paths = require('../config/paths.json') // specify paths to main working directories
+const generateServiceWorker = require('../lib/build-service-worker') // setup service worker generation
 
 // setup synchronised browser testing
 metalsmith.use(browsersync({
+  open: true,
   server: paths.public, // server directory
+  port: 4000, // unique port for service worker on 'localhost'
   files: [
     paths.source + '**/*',
     paths.views + '**/*',
@@ -16,4 +18,6 @@ metalsmith.use(browsersync({
 // build to destination directory
 metalsmith.build(function (err, files) {
   if (err) { throw err }
+  // after all files are built, generate the service worker
+  generateServiceWorker.init()
 })
