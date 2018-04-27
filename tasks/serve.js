0171b8b3fd528ce4e5865f9f4a3e563369d390e8
Joe Lanman <joelanman@gmail.com>
2018-04-27 16:43:23 +0100

    fix backlog work, use dotenv for private keys

--- tasks/serve.js
+++ tasks/serve.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
+
 const fs = require('fs')
 
 const browsersync = require('metalsmith-browser-sync') // setup synchronised browser testing
-const metalsmith = require('../lib/metalsmith') // configured static site generator
 
 const paths = require('../config/paths.json') // specify paths to main working directories
 
@@ -16,7 +17,7 @@ const github = new GithubGraphQLApi({
 github.query(`
 query {
   repository(owner:"alphagov", name:"govuk-design-system-backlog") {
-    project(number:3){
+    project(number:1){
       name,
       columns(first:3){
         nodes{
@@ -43,8 +44,10 @@ query {
 }
 `, null, (res, err) => {
  	fs.writeFileSync('data/backlog.json', JSON.stringify(res, null, 2))
-  	
-  	// setup synchronised browser testing
+
+  const metalsmith = require('../lib/metalsmith') // configured static site generator
+
+	// setup synchronised browser testing
 	metalsmith.use(browsersync({
 	  server: paths.public, // server directory
 	  files: [paths.source + '**/*', paths.views + '**/*'] // files to watch
@@ -56,6 +59,3 @@ query {
 	})
 
 })
-
-
-
