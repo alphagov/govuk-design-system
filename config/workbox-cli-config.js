7a69dd207647e05ea203eeeebf5ae431dc0ae586
Matt Hobbs <matt@nooshu.com>
2018-05-04 14:48:24 +0100

    Now use injectManifest and fix navigation '/' issue

--- config/workbox-cli-config.js
+++ config/workbox-cli-config.js
@@ -1,36 +1,8 @@
 const paths = require('./paths.json') // specify paths to main working directories
 
 module.exports = {
-  globDirectory: paths.public,
-  globPatterns: [
-    '**/*.{html,js,css,png,jpg}'
-  ],
+  swSrc: 'lib/service-worker-src.js',
   swDest: paths.public + 'service-worker.js',
-  importWorkboxFrom: 'local',
-  skipWaiting: true,
-  clientsClaim: true,
-  directoryIndex: 'index.html',
-  cacheId: 'govuk-ds',
-  runtimeCaching: [{
-    urlPattern: /.*/,
-    handler: 'networkFirst'
-  },
-  {
-    urlPattern: new RegExp('https://www.google-analytics.com/analytics.js'),
-    handler: 'networkFirst',
-    options: {
-      expiration: {
-        maxAgeSeconds: 60 * 60
-      }
-    }
-  },
-  {
-    urlPattern: new RegExp('https://www.googletagmanager.com/gtm.js'),
-    handler: 'networkFirst',
-    options: {
-      expiration: {
-        maxAgeSeconds: 60 * 60
-      }
-    }
-  }]
+  globDirectory: paths.public,
+  globPatterns: ['**/*.{html,js,css,png,jpg,gif}']
 }
