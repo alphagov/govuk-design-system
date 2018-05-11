fca1dcc53a2077980c902354a0fe67258661e598
Joe Lanman <joelanman@gmail.com>
2018-05-11 14:12:41 +0100

    fix serve task - run backlog first, then metalsmith

--- tasks/serve.js
+++ tasks/serve.js
@@ -1,20 +1,21 @@
-const browsersync = require('metalsmith-browser-sync') // setup synchronised browser testing
-const metalsmith = require('../lib/metalsmith') // configured static site generator
-
 const githubBacklog = require('../lib/github-backlog') // get backlog data from GitHubAPI
-const paths = require('../config/paths.json') // specify paths to main working directories
-
-// setup synchronised browser testing
-metalsmith.use(browsersync({
-  server: paths.public, // server directory
-  files: [
-    paths.source + '**/*',
-    paths.views + '**/*',
-    'node_modules/@govuk-frontend/**/*'
-  ] // files to watch
-}))
 
 githubBacklog.getData(() => {
+
+  const browsersync = require('metalsmith-browser-sync') // setup synchronised browser testing
+  const metalsmith = require('../lib/metalsmith') // configured static site generator
+  const paths = require('../config/paths.json') // specify paths to main working directories
+
+  // setup synchronised browser testing
+  metalsmith.use(browsersync({
+    server: paths.public, // server directory
+    files: [
+      paths.source + '**/*',
+      paths.views + '**/*',
+      'node_modules/@govuk-frontend/**/*'
+    ] // files to watch
+  }))
+
   // build to destination directory
   metalsmith.build(function (err, files) {
     if (err) { throw err }
