23ad292b1b2e22f2415c3a2b517630f9a6bc258a
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2018-05-22 18:07:45 +0100

    Build GOV.UK Frontend JavaScript
    Intentionally avoids importing Error Summary JavaScript to avoid,
    stealing focus and scrolling the page.

---
+++ src/javascripts/govuk-frontend.js
@@ -0,0 +1,19 @@
+import Button from '@govuk-frontend/frontend/components/button/button'
+import Details from '@govuk-frontend/frontend/components/details/details'
+import Checkboxes from '@govuk-frontend/frontend/components/checkboxes/checkboxes'
+// Intentionally avoid importing ErrorSummary since it will steal focus, scrolling example pages.
+// import ErrorSummary from '@govuk-frontend/frontend/components/error-summary/error-summary'
+import Radios from '@govuk-frontend/frontend/components/radios/radios'
+
+new Button().init()
+new Details().init()
+
+var $checkbox = document.querySelector('[data-module="checkboxes"]')
+if ($checkbox) {
+  new Checkboxes($checkbox).init()
+}
+
+var $radio = document.querySelector('[data-module="radios"]')
+if ($radio) {
+  new Radios($radio).init()
+}
