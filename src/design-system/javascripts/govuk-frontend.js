fe18017e1409fd5abaa70999fe45e2b43083015c
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2018-06-15 09:39:05 +0100

    Migrate to the new govuk-frontend package name

--- src/design-system/javascripts/govuk-frontend.js
+++ src/design-system/javascripts/govuk-frontend.js
@@ -1,9 +1,9 @@
-import Button from '@govuk-frontend/frontend/components/button/button'
-import Details from '@govuk-frontend/frontend/components/details/details'
-import Checkboxes from '@govuk-frontend/frontend/components/checkboxes/checkboxes'
+import Button from 'govuk-frontend/components/button/button'
+import Details from 'govuk-frontend/components/details/details'
+import Checkboxes from 'govuk-frontend/components/checkboxes/checkboxes'
 // Intentionally avoid importing ErrorSummary since it will steal focus, scrolling example pages.
-// import ErrorSummary from '@govuk-frontend/frontend/components/error-summary/error-summary'
-import Radios from '@govuk-frontend/frontend/components/radios/radios'
+// import ErrorSummary from 'govuk-frontend/components/error-summary/error-summary'
+import Radios from 'govuk-frontend/components/radios/radios'
 
 new Button(document).init()
 
