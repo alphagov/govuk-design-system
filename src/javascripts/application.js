2fef10cd6e558999f45ffcd8281042dfd799a0b7
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2018-08-16 16:31:42 +0100

    Update Copy component to use modules
    Requires updating the clipboard dependency to allow imports

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,6 +1,7 @@
 import CookieBanner from './components/cookie-banner.js'
 import Example from './components/example.js'
 import Tabs from './components/tabs.js'
+import Copy from './components/copy.js'
 
 // Add cookie message
 CookieBanner.addCookieMessage()
@@ -10,3 +11,6 @@ Example.init('.js-example__frame')
 
 // Initialise tabs
 Tabs.init('.js-example')
+
+// Add copy to clipboard to code blocks inside tab containers
+Copy.init('.app-tabs__container pre')
