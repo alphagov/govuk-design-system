675a2084c7db8f58df22dcc83b8413e7f18e446d
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2018-08-16 16:30:23 +0100

    Update Tabs component to use modules

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,8 +1,12 @@
 import CookieBanner from './components/cookie-banner.js'
 import Example from './components/example.js'
+import Tabs from './components/tabs.js'
 
 // Add cookie message
 CookieBanner.addCookieMessage()
 
 // Initialise example frames
 Example.init('.js-example__frame')
+
+// Initialise tabs
+Tabs.init('.js-example')
--- src/javascripts/components/tabs.js
+++ src/javascripts/components/tabs.js
@@ -1,102 +1,98 @@
-;(function (global) {
-  'use strict'
-  var $ = global.jQuery
-  var GOVUK = global.GOVUK || {}
+import $ from 'jquery'
 
-  GOVUK.tabs = {
+var Tabs = {
 
-    // Reset tabs and containers
-    resetTabs: function (id) {
-      // Check if we have an id
-      if (!id) {
-        console.error('id is undefined')
-        return
+  // Reset tabs and containers
+  resetTabs: function (id) {
+    // Check if we have an id
+    if (!id) {
+      console.error('id is undefined')
+      return
+    }
+
+    var $example = $(id).parent()
+
+    // Reset state
+    $example.find('.js-tabs__item').removeClass('app-tabs__item--current')
+    $example.find('.js-tabs__heading').removeClass('app-tabs__heading--current')
+    $example.find('.js-tabs__item a').removeAttr('aria-selected')
+    $example.find('.js-tabs__container').hide().attr('aria-hidden', 'true')
+  },
+
+  // Activate current tab
+  activateCurrentTab: function (id) {
+    // Check if we have an id
+    if (!id) {
+      console.error('id is undefined')
+      return
+    }
+
+    // Reset tabs
+    Tabs.resetTabs(id)
+
+    // Set current active tab for both tabs and accordion links
+    $('[href="' + id + '"]').attr('aria-selected', 'true')
+    var $parents = $('[href="' + id + '"]').parent()
+    $.each($parents, function (key, obj) {
+      if ($(obj).hasClass('app-tabs__item')) {
+        $(obj).addClass('app-tabs__item--current')
+      } else if ($(obj).hasClass('app-tabs__heading')) {
+        $(obj).addClass('app-tabs__heading--current')
       }
+    })
 
-      var $example = $(id).parent()
+    // Set current container
+    $(id).show().removeAttr('aria-hidden')
+  },
 
-      // Reset state
-      $example.find('.js-tabs__item').removeClass('app-tabs__item--current')
-      $example.find('.js-tabs__heading').removeClass('app-tabs__heading--current')
-      $example.find('.js-tabs__item a').removeAttr('aria-selected')
-      $example.find('.js-tabs__container').hide().attr('aria-hidden', 'true')
-    },
+  // Tabs mode
+  clickTabItem: function (e) {
+    e.preventDefault()
+
+    // Get tab container ID
+    var id = $(this).attr('href')
 
     // Activate current tab
-    activateCurrentTab: function (id) {
-      // Check if we have an id
-      if (!id) {
-        console.error('id is undefined')
-        return
+    Tabs.activateCurrentTab(id)
+  },
+
+  // Close current container on click
+  clickCloseContainer: function (e) {
+    e.preventDefault()
+
+    // Get tab container ID and save example object
+    var id = $(this).parent().attr('id')
+
+    // Reset tabs
+    Tabs.resetTabs('#' + id)
+  },
+
+  init: function (selector) {
+    // If more than one container
+    var $examples = $(selector)
+    $.each($examples, function (key, obj) {
+      if ($(obj).find('.js-tabs__container').length > 1) {
+        // Hide all containers
+        $(obj).find('.js-tabs__container').hide()
+
+        // Add close button to each container
+        $(obj).find('.js-tabs__container').append('<button class="app-tabs__close js-tabs__close">Close</button>')
+        $(obj).find('.js-tabs__container').addClass('app-tabs__container--with-close-button')
       }
+    })
 
-      // Reset tabs
-      GOVUK.tabs.resetTabs(id)
+    // Bind tab item click
+    $('.js-tabs__item a').click(Tabs.clickTabItem)
 
-      // Set current active tab for both tabs and accordion links
-      $('[href="' + id + '"]').attr('aria-selected', 'true')
-      var $parents = $('[href="' + id + '"]').parent()
-      $.each($parents, function (key, obj) {
-        if ($(obj).hasClass('app-tabs__item')) {
-          $(obj).addClass('app-tabs__item--current')
-        } else if ($(obj).hasClass('app-tabs__heading')) {
-          $(obj).addClass('app-tabs__heading--current')
-        }
-      })
+    // Bind accordion item click
+    $('.js-tabs__heading a').click(Tabs.clickTabItem)
 
-      // Set current container
-      $(id).show().removeAttr('aria-hidden')
-    },
+    // Bind close container link
+    $('.js-tabs__close').click(Tabs.clickCloseContainer)
 
-    // Tabs mode
-    clickTabItem: function (e) {
-      e.preventDefault()
-
-      // Get tab container ID
-      var id = $(this).attr('href')
-
-      // Activate current tab
-      GOVUK.tabs.activateCurrentTab(id)
-    },
-
-    // Close current container on click
-    clickCloseContainer: function (e) {
-      e.preventDefault()
-
-      // Get tab container ID and save example object
-      var id = $(this).parent().attr('id')
-
-      // Reset tabs
-      GOVUK.tabs.resetTabs('#' + id)
-    },
-
-    init: function (selector) {
-      // If more than one container
-      var $examples = $(selector)
-      $.each($examples, function (key, obj) {
-        if ($(obj).find('.js-tabs__container').length > 1) {
-          // Hide all containers
-          $(obj).find('.js-tabs__container').hide()
-
-          // Add close button to each container
-          $(obj).find('.js-tabs__container').append('<button class="app-tabs__close js-tabs__close">Close</button>')
-          $(obj).find('.js-tabs__container').addClass('app-tabs__container--with-close-button')
-        }
-      })
-
-      // Bind tab item click
-      $('.js-tabs__item a').click(GOVUK.tabs.clickTabItem)
-
-      // Bind accordion item click
-      $('.js-tabs__heading a').click(GOVUK.tabs.clickTabItem)
-
-      // Bind close container link
-      $('.js-tabs__close').click(GOVUK.tabs.clickCloseContainer)
-
-      // Show open containers
-      $('.js-tabs__heading--open a').click()
-    }
+    // Show open containers
+    $('.js-tabs__heading--open a').click()
   }
+}
 
-  global.GOVUK = GOVUK
-})(window); // eslint-disable-line semi
+export default Tabs
