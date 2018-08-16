d78e886e5613da5b2e9e663794b76ca2ba282c89
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2018-08-16 16:25:11 +0100

    Update Example component using modules

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,4 +1,8 @@
 import CookieBanner from './components/cookie-banner.js'
+import Example from './components/example.js'
 
 // Add cookie message
 CookieBanner.addCookieMessage()
+
+// Initialise example frames
+Example.init('.js-example__frame')
--- src/javascripts/components/example.js
+++ src/javascripts/components/example.js
@@ -1,19 +1,18 @@
-;(function (global) {
-  'use strict'
-  var $ = global.jQuery
-  var GOVUK = global.GOVUK || {}
+// The iframe-resizer module seems to export it's methods in an odd way
+// So we use an underscore here.
+import _ from 'iframe-resizer'
 
-  GOVUK.example = {
-    init: function (selector) {
-      try {
-        // Example iframe; set the height equal to the body height
-        $(selector).iFrameResize({scrolling: 'auto', autoResize: true})
-      } catch (err) {
-        if (err) {
-          console.error(err.message)
-        }
+var Example = {
+  init: function (selector) {
+    try {
+      // Example iframe; set the height equal to the body height
+      _.iframeResizer({ scrolling: 'auto', autoResize: true }, selector)
+    } catch (err) {
+      if (err) {
+        console.error(err.message)
       }
     }
   }
-  global.GOVUK = GOVUK
-})(window); // eslint-disable-line semi
+}
+
+export default Example
