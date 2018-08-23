d078c0b79f1b5f3fabc8bfadaee702dbd1249750
igloosi <jani.kraner@gmail.com>
2018-08-23 15:17:17 +0100

    Change Example initialisation
    To follow GOV.UK Frontend convetions

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,3 +1,4 @@
+import common from 'govuk-frontend/common'
 import CookieBanner from './components/cookie-banner.js'
 import Example from './components/example.js'
 import Tabs from './components/tabs.js'
@@ -5,11 +6,16 @@ import Copy from './components/copy.js'
 import MobileNav from './components/mobile-navigation.js'
 import Search from './components/search.js'
 
+var nodeListForEach = common.nodeListForEach
+
 // Add cookie message
 CookieBanner.addCookieMessage()
 
 // Initialise example frames
-Example.init('.js-example__frame')
+var $examples = document.querySelectorAll('[data-module="app-example-frame"]')
+nodeListForEach($examples, function ($example) {
+  new Example($example).init()
+})
 
 // Initialise tabs
 Tabs.init('.js-example')
