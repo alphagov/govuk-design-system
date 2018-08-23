2162d37cfd9c9678fc103a791cd6460934e75ca7
igloosi <jani.kraner@gmail.com>
2018-08-23 11:35:08 +0100

    Rewrite copy functionality
    Removes jQuery dependency
    Followes GOV.UK Frontend conventions

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,9 +1,12 @@
+import common from 'govuk-frontend/common'
 import CookieBanner from './components/cookie-banner.js'
 import Example from './components/example.js'
 import Tabs from './components/tabs.js'
 import Copy from './components/copy.js'
 import MobileNav from './components/mobile-navigation.js'
 import Search from './components/search.js'
+
+var nodeListForEach = common.nodeListForEach
 
 // Add cookie message
 CookieBanner.addCookieMessage()
@@ -15,7 +18,10 @@ Example.init('.js-example__frame')
 Tabs.init('.js-example')
 
 // Add copy to clipboard to code blocks inside tab containers
-Copy.init('.app-tabs__container pre')
+var $codeBlocks = document.querySelectorAll('[data-module="app-copy"]')
+nodeListForEach($codeBlocks, function ($codeBlock) {
+  new Copy($codeBlock).init()
+})
 
 // Initialise mobile navigation
 MobileNav.init()
--- src/javascripts/components/copy.js
+++ src/javascripts/components/copy.js
@@ -1,26 +1,41 @@
-import $ from 'jquery'
+import 'govuk-frontend/vendor/polyfills/Event'
 import ClipboardJS from 'clipboard'
 
-var Copy = {
-  init: function (selector) {
-    $(selector).parent().prepend('<button class="app-copy-button js-copy-button" aria-live="assertive">Copy</button>')
-    // Copy to clipboard
-    try {
-      new ClipboardJS('.js-copy-button', {
-        target: function (trigger) {
-          return trigger.nextElementSibling
-        }
-      }).on('success', function (e) {
-        e.trigger.textContent = 'Copied'
-        e.clearSelection()
-        setTimeout(function () {
-          e.trigger.textContent = 'Copy'
-        }, 5000)
-      })
-    } catch (err) {
-      if (err) {
-        console.log(err.message)
+function Copy ($module) {
+  this.$module = $module
+}
+
+Copy.prototype.init = function () {
+  var $module = this.$module
+  if (!$module) {
+    return
+  }
+  var $button = document.createElement('button')
+  $button.className = 'app-copy-button js-copy-button'
+  $button.setAttribute('aria-live', 'assertive')
+  $button.textContent = 'Copy'
+
+  var $parent = $module.parentNode
+  $parent.insertBefore($button, $parent.firstChild)
+  this.copyAction()
+}
+Copy.prototype.copyAction = function () {
+  // Copy to clipboard
+  try {
+    new ClipboardJS('.js-copy-button', {
+      target: function (trigger) {
+        return trigger.nextElementSibling
       }
+    }).on('success', function (e) {
+      e.trigger.textContent = 'Copied'
+      e.clearSelection()
+      setTimeout(function () {
+        e.trigger.textContent = 'Copy'
+      }, 5000)
+    })
+  } catch (err) {
+    if (err) {
+      console.log(err.message)
     }
   }
 }
