2ba7eccf70c2cee502d9eafe4fb4e73f6a52f371
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2018-10-11 16:55:35 +0100

    Add tests for the search tracking

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -20,6 +20,10 @@ afterEach(async (done) => {
   done()
 })
 
+function timeout (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 describe('Site search', () => {
   it('does not return any results when searching for something that does not exist', async () => {
     await page.goto(baseUrl, { waitUntil: 'load' })
@@ -138,5 +142,159 @@ describe('Site search', () => {
     const $input = await page.evaluate(() => document.querySelector('.app-site-search__input'))
 
     expect($activeElement).toEqual($input)
+  })
+
+  describe('tracking', () => {
+    const SEARCH_TRACKING_TIMEOUT = 2000
+    it('should track if there are no results', async () => {
+      await page.goto(baseUrl, { waitUntil: 'load' })
+
+      await page.waitForSelector('.app-site-search__input')
+      await page.focus('.app-site-search__input')
+      await page.type('.app-site-search__input', 'lorem ipsum')
+      await timeout(SEARCH_TRACKING_TIMEOUT)
+      const GoogleTagManagerDataLayer = await page.evaluate(() => window.dataLayer)
+
+      expect(GoogleTagManagerDataLayer).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            ecommerce: {
+              impressions: []
+            },
+            event: 'site-search',
+            eventDetails: {
+              action: 'no result',
+              category: 'site search',
+              label: 'lorem ipsum'
+            }
+          })
+        ])
+      )
+    })
+    it('should track if there are results', async () => {
+      await page.goto(baseUrl, { waitUntil: 'load' })
+
+      await page.waitForSelector('.app-site-search__input')
+      await page.focus('.app-site-search__input')
+      await page.type('.app-site-search__input', 'g')
+      await timeout(SEARCH_TRACKING_TIMEOUT)
+      const optionResults = await page.$$('.app-site-search__option')
+      const GoogleTagManagerDataLayer = await page.evaluate(() => window.dataLayer)
+
+      // Find layer that has the impressions to test.
+      const impressions =
+        GoogleTagManagerDataLayer
+          .filter(layer => layer.ecommerce)
+          .map(layer => layer.ecommerce.impressions)[0]
+
+      expect(impressions.length).toEqual(optionResults.length)
+      expect(GoogleTagManagerDataLayer).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            ecommerce: {
+              impressions: expect.arrayContaining([
+                expect.objectContaining({
+                  name: expect.any(String),
+                  category: expect.any(String),
+                  list: 'g',
+                  position: expect.any(Number)
+                })
+              ])
+            },
+            event: 'site-search',
+            eventDetails: {
+              action: 'results',
+              category: 'site search',
+              label: 'g'
+            }
+          })
+        ])
+      )
+    })
+    it('should track if a result is clicked', async () => {
+      await page.goto(baseUrl, { waitUntil: 'load' })
+
+      // Prevent page from unloading so we can check what was tracked.
+      await page.evaluate(() => {
+        window.onbeforeunload = () => true
+      })
+      page.on('dialog', async dialog => {
+        await dialog.dismiss()
+      })
+
+      await page.waitForSelector('.app-site-search__input')
+      await page.focus('.app-site-search__input')
+      await page.type('.app-site-search__input', 'g')
+      await page.keyboard.press('ArrowDown')
+      await page.keyboard.press('Enter')
+
+      const GoogleTagManagerDataLayer = await page.evaluate(() => window.dataLayer)
+
+      expect(GoogleTagManagerDataLayer).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            ecommerce: {
+              click: {
+                actionField: {
+                  list: 'g'
+                },
+                products: expect.arrayContaining([
+                  expect.objectContaining({
+                    name: expect.any(String),
+                    category: expect.any(String),
+                    list: 'g',
+                    position: 2
+                  })
+                ])
+              }
+            },
+            event: 'site-search',
+            eventDetails: {
+              action: 'click',
+              category: 'site search',
+              label: expect.stringContaining('g |')
+            }
+          })
+        ])
+      )
+    })
+    it('should block personally identifable information emails', async () => {
+      await page.goto(baseUrl, { waitUntil: 'load' })
+
+      await page.waitForSelector('.app-site-search__input')
+      await page.focus('.app-site-search__input')
+      await page.type('.app-site-search__input', 'user@example.com')
+      await timeout(SEARCH_TRACKING_TIMEOUT)
+      const GoogleTagManagerDataLayer = await page.evaluate(() => window.dataLayer)
+
+      expect(GoogleTagManagerDataLayer).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            eventDetails: expect.objectContaining({
+              label: '[blocked]'
+            })
+          })
+        ])
+      )
+    })
+    it('should block personally identifable information numbers', async () => {
+      await page.goto(baseUrl, { waitUntil: 'load' })
+
+      await page.waitForSelector('.app-site-search__input')
+      await page.focus('.app-site-search__input')
+      await page.type('.app-site-search__input', '079460999')
+      await timeout(SEARCH_TRACKING_TIMEOUT)
+      const GoogleTagManagerDataLayer = await page.evaluate(() => window.dataLayer)
+
+      expect(GoogleTagManagerDataLayer).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            eventDetails: expect.objectContaining({
+              label: '[blocked][blocked][blocked][blocked][blocked][blocked][blocked][blocked][blocked]'
+            })
+          })
+        ])
+      )
+    })
   })
 })
