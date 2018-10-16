d5256eee053b48e6ba6d08abaaaa0bb8ae849111
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2018-10-16 13:40:46 +0100

    Add tabs to IE8 build

--- src/javascripts/application-ie8.js
+++ src/javascripts/application-ie8.js
@@ -1,6 +1,7 @@
 import common from 'govuk-frontend/common'
 import CookieBanner from './components/cookie-banner.js'
 import Example from './components/example.js'
+import Tabs from './components/tabs.js'
 
 var nodeListForEach = common.nodeListForEach
 
@@ -12,3 +13,6 @@ var $examples = document.querySelectorAll('[data-module="app-example-frame"]')
 nodeListForEach($examples, function ($example) {
   new Example($example).init()
 })
+
+// Initialise tabs
+Tabs.init('.js-example')
