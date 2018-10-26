e3a2af6a75c4dd8bd6b5fc3352ac42cc70c7efff
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2018-10-26 12:40:48 +0100

    Open options table when detected in URL hash
    Check for "nunjucks-options-[table-name]" in the hash.
    
    If detected, open the Nunjucks tab and the details element
    to show user the options table directly.

--- src/javascripts/application-ie8.js
+++ src/javascripts/application-ie8.js
@@ -2,6 +2,7 @@ import common from 'govuk-frontend/common'
 import CookieBanner from './components/cookie-banner.js'
 import Example from './components/example.js'
 import AppTabs from './components/tabs.js'
+import OptionsTable from './components/options-table.js'
 
 var nodeListForEach = common.nodeListForEach
 
@@ -19,3 +20,7 @@ var $tabs = document.querySelectorAll('[data-module="app-tabs"]')
 nodeListForEach($tabs, function ($tabs) {
   new AppTabs($tabs).init()
 })
+
+// Open options table when detected in URL hash
+// Do this after initialising tabs
+OptionsTable.init()
