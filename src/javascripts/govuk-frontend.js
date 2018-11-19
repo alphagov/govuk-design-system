f6220660b023f253ba0ff6a72d9c67676800e167
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2018-11-19 15:35:52 +0000

    Add ErrorSummary component JavaScript with override
    Override the init function to avoid the autofocus behaviour, we should remove this when the component has this option itself.

--- src/javascripts/govuk-frontend.js
+++ src/javascripts/govuk-frontend.js
@@ -2,8 +2,7 @@ import Button from 'govuk-frontend/components/button/button'
 import Details from 'govuk-frontend/components/details/details'
 import CharacterCount from 'govuk-frontend/components/character-count/character-count'
 import Checkboxes from 'govuk-frontend/components/checkboxes/checkboxes'
-// Intentionally avoid importing ErrorSummary since it will steal focus, scrolling example pages.
-// import ErrorSummary from 'govuk-frontend/components/error-summary/error-summary'
+import ErrorSummary from 'govuk-frontend/components/error-summary/error-summary'
 import Radios from 'govuk-frontend/components/radios/radios'
 import Header from 'govuk-frontend/components/header/header'
 import Tabs from 'govuk-frontend/components/tabs/tabs'
@@ -13,6 +12,18 @@ new Button(document).init()
 var $details = document.querySelector('details')
 if ($details) {
   new Details($details).init()
+}
+
+var $errorSummary = document.querySelector('[data-module="error-summary"]')
+if ($errorSummary) {
+  var errorSummary = new ErrorSummary($errorSummary)
+  // Override the `init` method since it automatically focuses the ErrorSummary.
+  // This is not ideal when showing examples for this component
+  // TODO: Allow option for ErrorSummary to avoid this hack
+  errorSummary.init = function () {
+    this.$module.addEventListener('click', ErrorSummary.prototype.handleClick.bind(this))
+  }
+  errorSummary.init()
 }
 
 var $characterCount = document.querySelector('[data-module="character-count"]')
