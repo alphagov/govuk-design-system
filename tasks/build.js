dd168a5c7d5222f8e421bd378099cb146cbf981d
Joe Lanman <joelanman@gmail.com>
2019-07-05 17:15:52 +0100

    change from env to task, only do link check on build not serve

--- tasks/build.js
+++ tasks/build.js
@@ -1,5 +1,8 @@
 const metalsmith = require('../lib/metalsmith') // configured static site generator
 
+// set task variable for metalsmith
+process.task = 'build'
+
 // build to destination directory
 metalsmith.build(function (err, files) {
   if (err) { throw err }
