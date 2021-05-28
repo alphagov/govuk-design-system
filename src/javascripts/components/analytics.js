8910c7115f6e90c97e5a4e0c7f60ff4325a17d6b
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2021-05-28 14:46:24 +0100

    Add GTM snippet

---
+++ src/javascripts/components/analytics.js
@@ -0,0 +1,18 @@
+export default function loadAnalytics () {
+  // Load gtm script
+  // Script based on snippet at https://developers.google.com/tag-manager/quickstart
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || []
+    w[l].push({
+      'gtm.start': new Date().getTime(),
+      'event': 'gtm.js'
+    })
+
+    var j = d.createElement(s)
+    var dl = l !== 'dataLayer' ? '&l=' + l : ''
+
+    j.async = true
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+    document.head.appendChild(j)
+  })(window, document, 'script', 'dataLayer', 'GTM-53XG2JT')
+}
