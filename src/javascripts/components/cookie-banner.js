edbc720c1abda2d4eb48e20c5985c348f69bcc3e
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2021-06-22 15:31:03 +0100

    Add the cookie banner Javascript
    Some key points:
    
    - The cookie banner is hidden by default, and only shown using JavaScript (so it is completely hidden for users with no JavaScript)
    - The cookie banner is only shown if there is no valid consent cookie already

---
+++ src/javascripts/components/cookie-banner.js
@@ -0,0 +1,89 @@
+import * as CookieFunctions from '../cookie-functions.js'
+import 'govuk-frontend/govuk/vendor/polyfills/Function/prototype/bind'
+import 'govuk-frontend/govuk/vendor/polyfills/Event'
+import common from 'govuk-frontend/govuk/common'
+
+var nodeListForEach = common.nodeListForEach
+
+var cookieBannerAcceptSelector = '.js-cookie-banner-accept'
+var cookieBannerRejectSelector = '.js-cookie-banner-reject'
+var cookieBannerHideButtonSelector = '.js-cookie-banner-hide'
+var cookieMessageSelector = '.js-cookie-banner-message'
+var cookieConfirmationAcceptSelector = '.js-cookie-banner-confirmation-accept'
+var cookieConfirmationRejectSelector = '.js-cookie-banner-confirmation-reject'
+
+function CookieBanner ($module) {
+  this.$module = $module
+}
+
+CookieBanner.prototype.init = function () {
+  this.$cookieBanner = this.$module
+  this.$acceptButton = this.$module.querySelector(cookieBannerAcceptSelector)
+  this.$rejectButton = this.$module.querySelector(cookieBannerRejectSelector)
+  this.$cookieMessage = this.$module.querySelector(cookieMessageSelector)
+  this.$cookieConfirmationAccept = this.$module.querySelector(cookieConfirmationAcceptSelector)
+  this.$cookieConfirmationReject = this.$module.querySelector(cookieConfirmationRejectSelector)
+  this.$cookieBannerHideButtons = this.$module.querySelectorAll(cookieBannerHideButtonSelector)
+
+  if (!this.$cookieBanner) {
+    return
+  }
+
+  // Show the cookie banner to users who have not consented or have an
+  // outdated consent cookie
+  var currentConsentCookie = CookieFunctions.getConsentCookie()
+
+  if (!currentConsentCookie || !CookieFunctions.isValidConsentCookie(currentConsentCookie)) {
+    // If the consent cookie version is not valid, we need to remove any cookies which have been
+    // set previously
+    CookieFunctions.resetCookies()
+
+    this.$cookieBanner.removeAttribute('hidden')
+  }
+
+  this.$acceptButton.addEventListener('click', this.acceptCookies.bind(this))
+  this.$rejectButton.addEventListener('click', this.rejectCookies.bind(this))
+
+  nodeListForEach(this.$cookieBannerHideButtons, function ($cookieBannerHideButton) {
+    $cookieBannerHideButton.addEventListener('click', this.hideBanner.bind(this))
+  }.bind(this))
+}
+
+CookieBanner.prototype.hideBanner = function () {
+  this.$cookieBanner.setAttribute('hidden', true)
+}
+
+CookieBanner.prototype.acceptCookies = function () {
+  // Do actual cookie consent bit
+  CookieFunctions.setConsentCookie({ analytics: true })
+
+  // Hide banner and show confirmation message
+  this.$cookieMessage.setAttribute('hidden', true)
+  this.revealConfirmationMessage(this.$cookieConfirmationAccept)
+}
+
+CookieBanner.prototype.rejectCookies = function () {
+  // Do actual cookie consent bit
+  CookieFunctions.setConsentCookie({ analytics: false })
+
+  // Hide banner and show confirmation message
+  this.$cookieMessage.setAttribute('hidden', true)
+  this.revealConfirmationMessage(this.$cookieConfirmationReject)
+}
+
+CookieBanner.prototype.revealConfirmationMessage = function (confirmationMessage) {
+  confirmationMessage.removeAttribute('hidden')
+
+  // Set tabindex to -1 to make the confirmation banner focusable with JavaScript
+  if (!confirmationMessage.getAttribute('tabindex')) {
+    confirmationMessage.setAttribute('tabindex', '-1')
+
+    confirmationMessage.addEventListener('blur', function () {
+      confirmationMessage.removeAttribute('tabindex')
+    })
+  }
+
+  confirmationMessage.focus()
+}
+
+export default CookieBanner
