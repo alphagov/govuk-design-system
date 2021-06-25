a3072235ca2d7142f5e66e5152cdca0506e06318
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2021-06-25 15:56:03 +0100

    Add cookie preferences form
    This commit adds a "form" to the cookies page for users to be able to
    update their cookie consent preferences.
    
    As the Design System website is a static site without a backend to
    process form submissions, this page uses JavaScript to process the form
    input, show notification banners, and update the cookies.  We
    only use cookies with JavaScript, so it is acceptable for users to not
    be able to update their cookie preferences if they do not have
    JavaScript enabled. In the case where JavaScript is not available the
    form is not visible.
    
    The cookie page has several dynamic components that are controlled by
    JavaScript:
    
    - a success notification banner which is shown only after cookie
    settings have been saved
    
    - a set of radios for each cookie type
    
    - a button which is used to submit the form
    
    Currently only one cookie type is supported, analytics, but the
    JavaScript in `src/javascripts/components/cookies-page.js` should be
    flexible enough to be expanded to support multiple cookie types without
    too much additional code.

---
+++ __tests__/cookies-page.test.js
@@ -0,0 +1,126 @@
+/* eslint-env jest */
+
+const { setupPage } = require('../lib/jest-utilities.js')
+const configPaths = require('../config/paths.json')
+const PORT = configPaths.testPort
+
+let page
+const baseUrl = 'http://localhost:' + PORT
+
+const cookiesPageSelector = '[data-module="app-cookies-page"]'
+
+beforeAll(async () => {
+  page = await setupPage()
+})
+
+afterAll(async () => {
+  await page.close()
+})
+
+describe('Cookies page', () => {
+  beforeEach(async () => {
+    await page.goto(`${baseUrl}/cookies`)
+  })
+
+  afterEach(async () => {
+    await page.deleteCookie({ name: 'design_system_cookies_policy' })
+    await page.setJavaScriptEnabled(true)
+  })
+
+  it('without JavaScript it has no visible inputs', async () => {
+    await page.setJavaScriptEnabled(false)
+    await page.goto(`${baseUrl}/cookies`)
+
+    const isAnalyticsFormGroupHidden = await page.waitForSelector(
+      cookiesPageSelector + ' .govuk-form-group input[type="radio"][name="analytics"]', { hidden: true }
+    )
+    expect(isAnalyticsFormGroupHidden).toBeTruthy()
+
+    const isSaveButtonHidden = await page.waitForSelector(
+      cookiesPageSelector + ' button', { hidden: true }
+    )
+    expect(isSaveButtonHidden).toBeTruthy()
+  })
+
+  it('has radios for each cookie type', async () => {
+    const isAnalyticsFormGroupVisible = await page.waitForSelector(
+      cookiesPageSelector + ' .govuk-form-group input[type="radio"][name="analytics"]', { visible: true }
+    )
+    expect(isAnalyticsFormGroupVisible).toBeTruthy()
+  })
+
+  it('sets the default radio selection to "no"', async () => {
+    expect(await page.cookies()).toEqual([])
+
+    const isAnalyticsDisagreeSelected = await page.waitForSelector(cookiesPageSelector + ' input[name="analytics"][value="no"]:checked', { visible: true })
+    expect(isAnalyticsDisagreeSelected).toBeTruthy()
+  })
+
+  it('has a save button', async () => {
+    const isSaveButtonVisible = await page.waitForSelector(
+      cookiesPageSelector + ' button', { visible: true }
+    )
+    expect(isSaveButtonVisible).toBeTruthy()
+  })
+
+  it('shows success notification banner after preferences are saved', async () => {
+    const isSuccessNotificationHidden = await page.waitForSelector(
+      cookiesPageSelector + ' .govuk-notification-banner--success', { hidden: true }
+    )
+    expect(isSuccessNotificationHidden).toBeTruthy()
+
+    await page.click(cookiesPageSelector + ' input[name="analytics"]')
+    await page.click(cookiesPageSelector + ' button')
+
+    const isSuccessNotificationVisible = await page.waitForSelector(
+      cookiesPageSelector + ' .govuk-notification-banner--success', { visible: true }
+    )
+    expect(isSuccessNotificationVisible).toBeTruthy()
+  })
+
+  it('saves user preferences to a cookie', async () => {
+    await page.click(cookiesPageSelector + ' input[name="analytics"][value="yes"]')
+    await page.click(cookiesPageSelector + ' button')
+
+    expect(await page.cookies()).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: 'design_system_cookies_policy',
+          value: '{"analytics":true,"version":1}'
+        })
+      ])
+    )
+
+    await page.click(cookiesPageSelector + ' input[name="analytics"][value="no"]')
+    await page.click(cookiesPageSelector + ' button')
+
+    expect(await page.cookies()).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: 'design_system_cookies_policy',
+          value: '{"analytics":false,"version":1}'
+        })
+      ])
+    )
+  })
+
+  it('shows the users existing preferences when the page is loaded', async () => {
+    await page.click(cookiesPageSelector + ' input[name="analytics"][value="no"]')
+    await page.click(cookiesPageSelector + ' button')
+
+    await page.goto(`${baseUrl}/cookies`)
+
+    const isAnalyticsDisagreeSelected = await page.waitForSelector(cookiesPageSelector + ' input[name="analytics"][value="no"]:checked', { visible: true })
+    expect(isAnalyticsDisagreeSelected).toBeTruthy()
+
+    await page.click(cookiesPageSelector + ' input[name="analytics"][value="yes"]')
+    await page.click(cookiesPageSelector + ' button')
+
+    await page.goto(`${baseUrl}/cookies`)
+
+    const isAnalyticsAgreeSelected = await page.waitForSelector(
+      cookiesPageSelector + ' input[name="analytics"][value="yes"]:checked', { visible: true }
+    )
+    expect(isAnalyticsAgreeSelected).toBeTruthy()
+  })
+})
