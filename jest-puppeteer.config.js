d7c7db968b105b96638e31ac3f3d950fe49b9d88
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2021-06-30 11:12:04 +0100

    Use eleventy to serve pages for tests
    This is less efficient than previous method of using static server, but
    will do for now.

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,7 +3,7 @@ const PORT = configPaths.testPort
 
 module.exports = {
   server: {
-    command: `node tasks/test-serve.js`,
+    command: `eleventy --serve --port ${PORT}`,
     launchTimeout: 30000,
     port: PORT
   }
