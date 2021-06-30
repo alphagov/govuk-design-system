4f45c23699e730ab165a2533f90186345d40a56c
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2021-06-30 09:54:54 +0100

    Add govuk-frontend templates to Nunjucks template paths
    Now `npx eleventy` is failing because some custom globals are not
    available.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,21 @@
-module.exports = config => {
+const nunjucks = require('nunjucks')
+
+const paths = require('./config/paths.json') // specify paths to main working directories
+
+const templatePaths = [
+  paths.layouts,
+  paths.partials,
+  paths.components,
+  paths.govukfrontend
+]
+
+module.exports = eleventyConfig => {
+  const nunjucksEnvironment = new nunjucks.Environment(
+    new nunjucks.FileSystemLoader(templatePaths)
+  )
+
+  eleventyConfig.setLibrary('njk', nunjucksEnvironment)
+
   return {
     dir: {
       input: 'src',
