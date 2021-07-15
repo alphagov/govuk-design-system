9a9698cfc15350d97f8d969d588b644ac5030675
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2021-07-15 15:06:39 +0100

    Remove cookie banner and cookie form support for IE8 and below
    Google Tag Manager doesn't officially support IE8 and below https://support.google.com/tagmanager/answer/4620708?hl=en
    
    January 30, 2018
    Tag Manager no longer officially supports container functionality in Microsoft Internet Explorer 8. Containers should load, but there may be inconsistencies in how some triggers and settings behave. Example: Link Click, Form Submit, and History Change triggers that use the Wait for Tags or Check Validation settings may not function as expected.
    
    We pull in separate JavaScript for IE8 and below, so if we never initialise the banner or form they will never be shown
    to users using these browsers.
    
    It's a shame that we can't collect data for these users, but we think this is probably
    a very small percentage of our users.

--- src/javascripts/application-ie8.js
+++ src/javascripts/application-ie8.js
@@ -1,22 +1,9 @@
 import common from 'govuk-frontend/govuk/common'
-import CookieBanner from './components/cookie-banner.js'
 import Example from './components/example.js'
 import AppTabs from './components/tabs.js'
 import OptionsTable from './components/options-table.js'
-import { getConsentCookie, isValidConsentCookie } from './components/cookie-functions.js'
-import Analytics from './components/analytics.js'
 
 var nodeListForEach = common.nodeListForEach
-
-// Initialise cookie banner
-var $cookieBanner = document.querySelector('[data-module="govuk-cookie-banner"]')
-new CookieBanner($cookieBanner).init()
-
-// Initialise analytics if consent is given
-var userConsent = getConsentCookie()
-if (userConsent && isValidConsentCookie(userConsent) && userConsent.analytics) {
-  Analytics()
-}
 
 // Initialise example frames
 var $examples = document.querySelectorAll('[data-module="app-example-frame"]')
