5fcea2224b37c23bc9c990e9af38a01f6476c74b
Vanita Barrett-Smith <vanita.barrett@digital.cabinet-office.gov.uk>
2022-07-08 13:52:14 +0100

    Basic homepage
    Note:
    
    - no JS functionality yet
    - some styling missing
    - links won't work

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function (config) {
   config.addWatchTarget("./src/assets/stylesheets/*.scss");
 
+  config.addPassthroughCopy({'node_modules/govuk-frontend/govuk/assets/fonts/*': 'assets/fonts/'});
+
+
   return {
     dir: {
       input: 'src',
