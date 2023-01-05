6cb85bc60b1f8d39fd350de76d5b574171d6e6a5
Brett Kyle <brett.kyle@digital.cabinet-office.gov.uk>
2023-01-05 20:51:00 +0000

    Add more missing filters and globals to eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 const nunjucks = require('nunjucks')
 
 const highlighter = require('./lib/highlighter.js')
-const { getFingerprint, getFrontmatter, getHTMLCode } = require('./lib/file-helper.js') // helper functions to operate on files
+const { getFingerprint, getFrontmatter, getHTMLCode, getNunjucksCode } = require('./lib/file-helper.js') // helper functions to operate on files
 const paths = require('./config/paths.json') // specify paths to main working directories
+const getMacroOptions = require('./lib/get-macro-options/index.js')
 
 const templatePaths = [
   paths.layouts,
@@ -17,9 +18,14 @@ module.exports = eleventyConfig => {
   )
 
   nunjucksEnvironment.addFilter('highlight', highlighter)
+  nunjucksEnvironment.addFilter('kebabCase', string => {
+    return string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
+  })
   nunjucksEnvironment.addGlobal('getFingerprint', getFingerprint)
   nunjucksEnvironment.addGlobal('getFrontmatter', getFrontmatter)
   nunjucksEnvironment.addGlobal('getHTMLCode', getHTMLCode)
+  nunjucksEnvironment.addGlobal('getMacroOptions', getMacroOptions)
+  nunjucksEnvironment.addGlobal('getNunjucksCode', getNunjucksCode)
 
   eleventyConfig.setLibrary('njk', nunjucksEnvironment)
 
