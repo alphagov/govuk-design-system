b5c6016c3ef9aa8b21b659646bbc9799725a887b
Colin Rotherham <work@colinr.com>
2023-01-13 14:09:19 +0000

    Run Puppeteer (Chromium) in ‘incognito’ context

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,9 @@
 const configPaths = require('./lib/paths.js')
+
 const PORT = configPaths.testPort
 
 module.exports = {
+  browserContext: 'incognito',
   server: {
     command: 'node tasks/test-serve.js',
     launchTimeout: 30000,
