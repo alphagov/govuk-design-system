ad2f9e90d5839f57e771c1b9cd1f02a713a2ba76
Colin Rotherham <work@colinr.com>
2023-02-27 15:19:21 +0000

    Enable Jest tests in `*.mjs` files

--- jest.config.mjs
+++ jest.config.mjs
@@ -15,6 +15,11 @@ export default {
   // Test timeout increased (5s to 15s)
   testTimeout: 15000,
 
+  // Enable Babel transforms until Jest supports ESM
+  // See: https://jestjs.io/docs/ecmascript-modules
+  testMatch: ['**/*.test.{js,mjs}'],
+  transform: { '^.+\\.m?js$': ['babel-jest'] },
+
   // Ignore built fixtures during `--watch`
   watchPathIgnorePatterns: ['fixtures/build']
 }
