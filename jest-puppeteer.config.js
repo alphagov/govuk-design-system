8c83b18d2d509c41c0d8d8800a4cd3d9b91e0b17
Colin Rotherham <work@colinr.com>
2023-05-12 17:58:38 +0100

    Align Puppeteer (+ Jest) configs with GOV.UK Frontend

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,29 @@
 const configPaths = require('./lib/paths.js')
 
+/**
+ * @type {import('jest-environment-puppeteer').JestPuppeteerConfig}
+ */
 module.exports = {
   browserContext: 'incognito',
+
+  /**
+   * Puppeteer launch options
+   */
+  launch: {
+    /**
+     * Allow headless mode switching using `HEADLESS=false`
+     * but default to `new` to skip deprecation warning
+     *
+     * {@link https://developer.chrome.com/articles/new-headless/}
+     */
+    headless: process.env.HEADLESS !== 'false'
+      ? 'new'
+      : false
+  },
+
+  /**
+   * Development server options
+   */
   server: {
     command: 'npm run serve',
     port: configPaths.port,
