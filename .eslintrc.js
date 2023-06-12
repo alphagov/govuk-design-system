e1e3871e2c7f014465b5b4c64614e287134f3f8c
Colin Rotherham <work@colinr.com>
2023-06-12 14:05:13 +0100

    Sync ESLint configs with `govuk-frontend`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,42 @@
 module.exports = {
   extends: 'standard',
   ignorePatterns: [
-    '**/fixtures/build/**'
+    '**/fixtures/build/**',
+
+    // Enable dotfile linting
+    '!.*',
+    'node_modules',
+    'node_modules/.*'
   ],
   overrides: [
+    {
+      extends: [
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:n/recommended',
+        'plugin:promise/recommended'
+      ],
+      files: ['**/*.{cjs,js,mjs}'],
+      parserOptions: {
+        ecmaVersion: 'latest'
+      },
+      plugins: [
+        'import',
+        'n',
+        'promise'
+      ]
+    },
     {
       files: ['**/*.test.{cjs,js,mjs}'],
       env: {
         jest: true
       },
       globals: {
-        page: true,
-        browser: true
+        page: 'readonly',
+        browser: 'readonly',
+        jestPuppeteer: 'readonly'
       }
     }
-  ]
+  ],
+  root: true
 }
--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,12 +1,17 @@
 module.exports = {
   overrides: [
     {
-      files: [
-        '**/*.{cjs,js,mjs}'
-      ],
+      files: ['**/*.mjs'],
+      excludedFiles: ['**/*.test.mjs'],
       env: {
         browser: true
       },
+      extends: ['plugin:es-x/restrict-to-es2015'],
+      parserOptions: {
+        // Note: Allow ES2015 for import/export syntax
+        ecmaVersion: '2015'
+      },
+      plugins: ['es-x'],
       rules: {
         'no-var': 'off',
         'object-shorthand': 'off',
