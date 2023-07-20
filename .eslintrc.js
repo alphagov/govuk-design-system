27f44bd14ef9517b11f2d719a179f4ee02035cca
Colin Rotherham <work@colinr.com>
2023-07-20 07:55:58 +0100

    Enable ESLint JSDoc checks

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
       extends: [
         'eslint:recommended',
         'plugin:import/recommended',
+        'plugin:jsdoc/recommended-typescript-flavor',
         'plugin:n/recommended',
         'plugin:promise/recommended'
       ],
@@ -22,6 +23,7 @@ module.exports = {
       },
       plugins: [
         'import',
+        'jsdoc',
         'n',
         'promise'
       ],
@@ -35,9 +37,47 @@ module.exports = {
           }
         ],
 
+        // JSDoc blocks are optional
+        'jsdoc/require-jsdoc': 'off',
+        'jsdoc/require-param-description': 'off',
+        'jsdoc/require-param': 'off',
+
+        // Check for valid formatting
+        'jsdoc/check-line-alignment': [
+          'warn',
+          'never', {
+            wrapIndent: '  '
+          }
+        ],
+
+        // Add unknown @jest-environment tag name
+        'jsdoc/check-tag-names': [
+          'warn', {
+            definedTags: ['jest-environment']
+          }
+        ],
+
+        // Require hyphens before param description
+        // Aligns with TSDoc style: https://tsdoc.org/pages/tags/param/
+        'jsdoc/require-hyphen-before-param-description': 'warn',
+
+        // Maintain new line after description
+        'jsdoc/tag-lines': [
+          'warn',
+          'never', {
+            startLines: 1
+          }
+        ],
+
         // Automatically use template strings
         'no-useless-concat': 'error',
         'prefer-template': 'error'
+      },
+      settings: {
+        jsdoc: {
+          // Allows us to use type declarations that exist in our dependencies
+          mode: 'typescript'
+        }
       }
     },
     {
