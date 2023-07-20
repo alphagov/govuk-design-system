34ae8a309f8d0a45a58e4b1da2e04b07a4705c95
Colin Rotherham <work@colinr.com>
2023-07-20 07:55:58 +0100

    Enable ESLint JSDoc checks

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -11,7 +11,27 @@ module.exports = {
         // Note: Allow ES2015 for import/export syntax
         ecmaVersion: '2015'
       },
-      plugins: ['es-x']
+      plugins: ['es-x'],
+      rules: {
+        // JSDoc blocks are mandatory in source code
+        'jsdoc/require-jsdoc': [
+          'error',
+          {
+            enableFixer: false,
+            require: {
+              ClassDeclaration: true,
+              ClassExpression: true,
+              FunctionExpression: true,
+              MethodDefinition: true
+            }
+          }
+        ],
+
+        // JSDoc @param required in (mandatory) blocks but
+        // @param description is necessary in source code
+        'jsdoc/require-param-description': 'warn',
+        'jsdoc/require-param': 'error'
+      }
     }
   ]
 }
