72735247c21da74be15acf1b2a2c262c7d7e601d
Colin Rotherham <work@colinr.com>
2023-07-20 07:40:57 +0100

    Consolidate GOV.UK Frontend into application scripts
    We need to ensure that only `initAll()` on example pages suppresses auto focus

---
+++ src/javascripts/application-example.mjs
@@ -0,0 +1,13 @@
+import { initAll } from 'govuk-frontend'
+
+import ExamplePage from './components/example-page.mjs'
+
+// Initialise GOV.UK Frontend
+initAll({
+  // Auto focusing the error summary and notification banner is not useful
+  // when used in examples, and causes the viewport to scroll
+  errorSummary: { disableAutoFocus: true },
+  notificationBanner: { disableAutoFocus: true }
+})
+
+new ExamplePage(document).init()
