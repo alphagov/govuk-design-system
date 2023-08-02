6c1b2d51e016bdf500be0222dda61fcf0e235b35
Colin Rotherham <work@colinr.com>
2023-08-02 10:43:11 +0100

    Ensure `.eslintrc.js` and dotfiles are excluded by Stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: 'stylelint-config-gds/scss',
-  ignoreFiles: ['**/*.{cjs,js,mjs}', 'deploy/public/**/*'],
+  ignoreFiles: ['**/?(.)*.{cjs,js,mjs}', 'deploy/public/**/*'],
   overrides: [
     {
       customSyntax: 'postcss-scss',
