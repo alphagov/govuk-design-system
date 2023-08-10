bc1db064aa275003449fefe37abfa41a14206e09
Colin Rotherham <work@colinr.com>
2023-08-10 14:54:00 +0100

    Temporarily disable component previews

--- src/javascripts/application.mjs
+++ src/javascripts/application.mjs
@@ -8,7 +8,6 @@ import CookieBanner from './components/cookie-banner.mjs'
 import { getConsentCookie, isValidConsentCookie } from './components/cookie-functions.mjs'
 import CookiesPage from './components/cookies-page.mjs'
 import Copy from './components/copy.mjs'
-import Example from './components/example.mjs'
 import Navigation from './components/navigation.mjs'
 import OptionsTable from './components/options-table.mjs'
 import Search from './components/search.mjs'
@@ -28,43 +27,6 @@ const userConsent = getConsentCookie()
 if (userConsent && isValidConsentCookie(userConsent) && userConsent.analytics) {
   Analytics()
 }
-
-// Register of examples by module
-const exampleRegister = /** @type {Map<Element, Example>} */ (
-  new Map()
-)
-
-// Defer init until viewport intersection
-const exampleObserver = 'IntersectionObserver' in window
-  ? new window.IntersectionObserver((entries) => {
-    for (const entry of entries) {
-      if (!entry.isIntersecting) {
-        return
-      }
-
-      console.log('init', { $module: entry.target })
-
-      // Stop observing, initialise
-      exampleObserver.unobserve(entry.target)
-      exampleRegister.get(entry.target).init()
-    }
-  })
-  : undefined
-
-// Initialise examples
-const $examples = document.querySelectorAll('[data-module="app-example"]')
-$examples.forEach(($example) => {
-  const example = new Example($example)
-
-  // Initialise iframes immediately
-  if (!exampleObserver) {
-    return example.init()
-  }
-
-  // Add to register, start observing
-  exampleRegister.set(example.$module, example)
-  exampleObserver.observe(example.$module)
-})
 
 // Initialise tabs
 const $tabs = document.querySelectorAll('[data-module="app-tabs"]')
