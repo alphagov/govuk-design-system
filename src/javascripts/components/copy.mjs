fa4610503835767028a4b5f53b0ac597cf95a00b
beeps (Kim Grey) <kimberly.grey@digital.cabinet-office.gov.uk>
2023-08-18 15:58:00 +0100

    Improve screen reader compatibility for code copied announcement
    Co-authored-by: Colin Rotherham <work@colinr.com>

--- src/javascripts/components/copy.mjs
+++ src/javascripts/components/copy.mjs
@@ -10,42 +10,65 @@ class Copy {
   constructor($module) {
     if (
       !($module instanceof HTMLElement) ||
-      !document.body.classList.contains('govuk-frontend-supported')
+      !document.body.classList.contains('govuk-frontend-supported') ||
+      !ClipboardJS.isSupported()
     ) {
       return this
     }
 
     this.$module = $module
 
+    /** @type {number | null} */
+    this.resetTimeoutId = null
+
     this.$button = document.createElement('button')
     this.$button.className = 'app-copy-button'
-    this.$button.setAttribute('aria-live', 'assertive')
     this.$button.textContent = 'Copy code'
 
+    this.$status = document.createElement('span')
+    this.$status.className = 'govuk-visually-hidden'
+    this.$status.setAttribute('aria-live', 'assertive')
+
+    this.$module.insertAdjacentElement('beforebegin', this.$status)
     this.$module.insertAdjacentElement('beforebegin', this.$button)
-    this.copyAction()
+
+    const $clipboard = new ClipboardJS(this.$button, {
+      target: (trigger) => trigger.nextElementSibling
+    })
+
+    $clipboard.on('success', (event) => this.successAction(event))
+    $clipboard.on('error', (event) => this.resetAction(event))
   }
 
   /**
-   * Set up button copy action
+   * Copy to clipboard success
+   *
+   * @param {import('clipboard').Event} event - Clipboard event
    */
-  copyAction() {
-    // Copy to clipboard
-    try {
-      new ClipboardJS(this.$button, {
-        target: (trigger) => trigger.nextElementSibling
-      }).on('success', (event) => {
-        this.$button.textContent = 'Code copied'
-        event.clearSelection()
-        setTimeout(() => {
-          this.$button.textContent = 'Copy code'
-        }, 5000)
-      })
-    } catch (error) {
-      if (error instanceof Error) {
-        console.log(error.message)
-      }
+  successAction(event) {
+    this.$button.textContent = this.$status.textContent = 'Code copied'
+    // Reset button after 5 seconds
+    this.resetAction(event, 5000)
+  }
+
+  /**
+   * Copy to clipboard reset
+   *
+   * @param {import('clipboard').Event} event - Clipboard event
+   * @param {number} [timeout] - Button text reset timeout
+   */
+  resetAction(event, timeout = 0) {
+    event.clearSelection()
+
+    if (this.resetTimeoutId) {
+      window.clearTimeout(this.resetTimeoutId)
     }
+
+    // Reset button after timeout
+    this.resetTimeoutId = window.setTimeout(() => {
+      this.$button.textContent = 'Copy code'
+      this.$status.textContent = ''
+    }, timeout)
   }
 }
 
