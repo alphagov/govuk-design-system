8c79f68db368e579c3850075e2e94def8dc08701
Colin Rotherham <work@colinr.com>
2023-09-27 13:51:35 +0100

    Use arrow functions to preserve `this` in components

--- src/javascripts/components/back-to-top.mjs
+++ src/javascripts/components/back-to-top.mjs
@@ -26,41 +26,35 @@ BackToTop.prototype.init = function () {
   let subNavIsIntersecting = false
   let subNavIntersectionRatio = 0
 
-  const observer = new window.IntersectionObserver(
-    function (entries) {
-      // Find the elements we care about from the entries
-      const footerEntry = entries.find(function (entry) {
-        return entry.target === $footer
-      })
-      const subNavEntry = entries.find(function (entry) {
-        return entry.target === $subNav
-      })
+  const observer = new window.IntersectionObserver((entries) => {
+    // Find the elements we care about from the entries
+    const footerEntry = entries.find((entry) => entry.target === $footer)
+    const subNavEntry = entries.find((entry) => entry.target === $subNav)
 
-      // If there is an entry this means the element has changed so lets check if it's intersecting.
-      if (footerEntry) {
-        footerIsIntersecting = footerEntry.isIntersecting
-      }
-      if (subNavEntry) {
-        subNavIsIntersecting = subNavEntry.isIntersecting
-        subNavIntersectionRatio = subNavEntry.intersectionRatio
-      }
+    // If there is an entry this means the element has changed so lets check if it's intersecting.
+    if (footerEntry) {
+      footerIsIntersecting = footerEntry.isIntersecting
+    }
+    if (subNavEntry) {
+      subNavIsIntersecting = subNavEntry.isIntersecting
+      subNavIntersectionRatio = subNavEntry.intersectionRatio
+    }
 
-      // If the subnav or the footer not visible then fix the back to top link to follow the user
-      if (subNavIsIntersecting || footerIsIntersecting) {
-        this.$module.classList.remove('app-back-to-top--fixed')
-      } else {
-        this.$module.classList.add('app-back-to-top--fixed')
-      }
+    // If the subnav or the footer not visible then fix the back to top link to follow the user
+    if (subNavIsIntersecting || footerIsIntersecting) {
+      this.$module.classList.remove('app-back-to-top--fixed')
+    } else {
+      this.$module.classList.add('app-back-to-top--fixed')
+    }
 
-      // If the subnav is visible but you can see it all at once, then a back to top link is likely not as useful.
-      // We hide the link but make it focusable for screen readers users who might still find it useful.
-      if (subNavIsIntersecting && subNavIntersectionRatio === 1) {
-        this.$module.classList.add('app-back-to-top--hidden')
-      } else {
-        this.$module.classList.remove('app-back-to-top--hidden')
-      }
-    }.bind(this)
-  )
+    // If the subnav is visible but you can see it all at once, then a back to top link is likely not as useful.
+    // We hide the link but make it focusable for screen readers users who might still find it useful.
+    if (subNavIsIntersecting && subNavIntersectionRatio === 1) {
+      this.$module.classList.add('app-back-to-top--hidden')
+    } else {
+      this.$module.classList.remove('app-back-to-top--hidden')
+    }
+  })
 
   observer.observe($footer)
   observer.observe($subNav)
