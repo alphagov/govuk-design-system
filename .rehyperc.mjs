d6b1a35f6b50ce12271907e85797db0e6767ad77
Owen Jones <owen.jones@digital.cabinet-office.gov.uk>
2024-04-22 12:55:13 +0100

    Get all unique classes via rehype

---
+++ .rehyperc.mjs
@@ -0,0 +1,36 @@
+import { writeFile } from 'node:fs/promises'
+
+import { matches } from 'hast-util-select'
+import { visit, EXIT } from 'unist-util-visit'
+
+const classes = new Set()
+
+export default {
+  plugins: [
+    () => async (tree) => {
+      visit(tree, (element, index, parent) => {
+        if (matches('app-example-page__wrapper', element)) {
+          return EXIT
+        }
+        if (matches('[class*="govuk-"]', element)) {
+          for (const className of element.properties.className) {
+            if (
+              className.includes('--') ||
+              className.includes('__') ||
+              !className.startsWith('govuk-')
+            ) {
+              continue
+            }
+
+            classes.add(className)
+          }
+        }
+      })
+
+      const classesArray = Array.from(classes)
+      classesArray.sort()
+
+      await writeFile('classes.log', classesArray.join('\n'))
+    }
+  ]
+}
