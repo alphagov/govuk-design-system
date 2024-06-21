7ca8d5077715f10dbc5cea84b486057564df9b7a
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2024-06-21 16:53:35 +0100

    Move `addToDataLayer` and `stripPossiblePII` to `analytics.mjs`
    These are not specifically related to search tracking, and more generic analytics utilities.
    Tree-shaking will take care of not including them in the bundle
    if they ever get unused.

--- src/javascripts/components/analytics.mjs
+++ src/javascripts/components/analytics.mjs
@@ -22,3 +22,38 @@ export function loadAnalytics() {
     })(window, document, 'script', 'dataLayer', 'GTM-53XG2JT')
   }
 }
+
+/**
+ * Push to Google Analytics
+ *
+ * @param {object} payload - Google Analytics payload
+ */
+export function addToDataLayer(payload) {
+  // @ts-expect-error Property does not exist on window
+  window.dataLayer = window.dataLayer || []
+  // @ts-expect-error Property does not exist on window
+  window.dataLayer.push(payload)
+}
+
+/**
+ * Strip possible personally identifiable information (PII)
+ *
+ * @param {string} string - Input string
+ * @returns {string} Output string
+ */
+export function stripPossiblePII(string) {
+  // Try to detect emails, postcodes, and NI numbers, and redact them.
+  // Regexes copied from GTM variable 'JS - Remove PII from Hit Payload'
+  string = string.replace(/[^\s=/?&]+(?:@|%40)[^\s=/?&]+/g, '[REDACTED EMAIL]')
+  string = string.replace(
+    /\b[A-PR-UWYZ][A-HJ-Z]?[0-9][0-9A-HJKMNPR-Y]?(?:[\s+]|%20)*[0-9](?!refund)[ABD-HJLNPQ-Z]{2,3}\b/gi,
+    '[REDACTED POSTCODE]'
+  )
+  string = string.replace(
+    /^\s*[a-zA-Z]{2}(?:\s*\d\s*){6}[a-zA-Z]?\s*$/g,
+    '[REDACTED NI NUMBER]'
+  )
+  // If someone has typed in a number it's likely not related so redact it
+  string = string.replace(/[0-9]+/g, '[REDACTED NUMBER]')
+  return string
+}
--- src/javascripts/components/search.tracking.mjs
+++ src/javascripts/components/search.tracking.mjs
@@ -1,37 +1,4 @@
-/**
- * Push to Google Analytics
- *
- * @param {object} payload - Google Analytics payload
- */
-function addToDataLayer(payload) {
-  // @ts-expect-error Property does not exist on window
-  window.dataLayer = window.dataLayer || []
-  // @ts-expect-error Property does not exist on window
-  window.dataLayer.push(payload)
-}
-
-/**
- * Strip possible personally identifiable information (PII)
- *
- * @param {string} string - Input string
- * @returns {string} Output string
- */
-function stripPossiblePII(string) {
-  // Try to detect emails, postcodes, and NI numbers, and redact them.
-  // Regexes copied from GTM variable 'JS - Remove PII from Hit Payload'
-  string = string.replace(/[^\s=/?&]+(?:@|%40)[^\s=/?&]+/g, '[REDACTED EMAIL]')
-  string = string.replace(
-    /\b[A-PR-UWYZ][A-HJ-Z]?[0-9][0-9A-HJKMNPR-Y]?(?:[\s+]|%20)*[0-9](?!refund)[ABD-HJLNPQ-Z]{2,3}\b/gi,
-    '[REDACTED POSTCODE]'
-  )
-  string = string.replace(
-    /^\s*[a-zA-Z]{2}(?:\s*\d\s*){6}[a-zA-Z]?\s*$/g,
-    '[REDACTED NI NUMBER]'
-  )
-  // If someone has typed in a number it's likely not related so redact it
-  string = string.replace(/[0-9]+/g, '[REDACTED NUMBER]')
-  return string
-}
+import { addToDataLayer, stripPossiblePII } from './analytics.mjs'
 
 /**
  * Track confirmed autocomplete result
