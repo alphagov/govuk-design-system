4466bd9e716c01c844d5ffa3f603450833e385c1
Patrick Cartlidge <patrickcartlidge@gmail.com>
2024-10-11 11:46:20 +0100

    Make `ExampleFrame` extend `Component`
    - Rename `this.$module` to `$this.$root`
    - Remove initialisation checks that occur in `Component`

--- src/javascripts/components/example-frame.mjs
+++ src/javascripts/components/example-frame.mjs
@@ -1,3 +1,4 @@
+import { Component } from 'govuk-frontend'
 import iFrameResize from 'iframe-resizer/js/iframeResizer.js'
 
 /**
@@ -7,30 +8,36 @@ import iFrameResize from 'iframe-resizer/js/iframeResizer.js'
  * template wrappers.
  *
  * @param {Element} $module - HTML element to use for example
+ * @augments Component<HTMLIFrameElement>
  */
-class ExampleFrame {
+class ExampleFrame extends Component {
+  /**
+   * Returns the root element of the component
+   *
+   * @returns {any} - the root element of component
+   */
+  get $root() {
+    // Unfortunately, govuk-frontend does not provide type definitions
+    // so TypeScript does not know of `this._$root`
+    // @ts-expect-error
+    return this._$root
+  }
+
   static moduleName = 'app-example-frame'
   /**
    * @param {Element} $module - HTML element
    */
   constructor($module) {
-    if (
-      !($module instanceof HTMLIFrameElement) ||
-      !document.body.classList.contains('govuk-frontend-supported')
-    ) {
-      return
-    }
-
-    this.$module = $module
+    super($module)
 
     // Initialise asap for eager iframes or browsers which don't support lazy loading
-    if (!('loading' in this.$module) || this.$module.loading !== 'lazy') {
-      return iFrameResize({ scrolling: 'omit' }, this.$module)
+    if (!('loading' in this.$root) || this.$root.loading !== 'lazy') {
+      return iFrameResize({ scrolling: 'omit' }, this.$root)
     }
 
-    this.$module.addEventListener('load', () => {
+    this.$root.addEventListener('load', () => {
       try {
-        iFrameResize({ scrolling: 'omit' }, this.$module)
+        iFrameResize({ scrolling: 'omit' }, this.$root)
       } catch (error) {
         if (error instanceof Error) {
           console.error(error.message)
