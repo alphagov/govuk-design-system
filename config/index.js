43274fde6b1cab43bb610312c59a4fb92ee0e79d
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2025-05-09 17:40:48 +0100

    Remove unnecessary `config/navigation.js` file

--- config/index.js
+++ config/index.js
@@ -1,12 +1,10 @@
 /**
  * Config
  */
-const navigation = require('./navigation')
 const paths = require('./paths')
 const ports = require('./ports')
 
 module.exports = {
-  navigation,
   paths,
   ports
 }
