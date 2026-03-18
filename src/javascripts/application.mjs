a4c857117fec88961609694f944b88ccd56e6078
Owen Jones <owen.jones@digital.cabinet-office.gov.uk>
2026-03-18 11:56:54 +0000

    Remove embed card component

--- src/javascripts/application.mjs
+++ src/javascripts/application.mjs
@@ -18,7 +18,6 @@ import {
 } from './components/cookie-functions.mjs'
 import CookiesPage from './components/cookies-page.mjs'
 import Copy from './components/copy.mjs'
-import EmbedCard from './components/embed-card.mjs'
 import ExampleFrame from './components/example-frame.mjs'
 import MobileNavigationSection from './components/mobile-navigation-section.mjs'
 import OptionsTable from './components/options-table.mjs'
@@ -65,46 +64,3 @@ createAll(Search)
 
 // Initialise back to top
 createAll(BackToTop)
-
-const $embedCards = document.querySelectorAll('[data-module="app-embed-card"]')
-
-const lazyEmbedObserver = new IntersectionObserver(function (
-  entries,
-  observer
-) {
-  entries.forEach(function (entry) {
-    if (entry.isIntersecting) {
-      try {
-        new EmbedCard(entry.target)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  })
-})
-
-$embedCards.forEach(function (lazyEmbed) {
-  lazyEmbedObserver.observe(lazyEmbed)
-})
-
-const campaignCookieBanner = document.querySelector(
-  '[data-cookie-category="campaign"]'
-)
-
-if (campaignCookieBanner) {
-  const callback = (mutationList, observer) => {
-    if (mutationList.length) {
-      $embedCards.forEach(function (lazyEmbed) {
-        lazyEmbedObserver.unobserve(lazyEmbed)
-        lazyEmbedObserver.observe(lazyEmbed)
-      })
-    }
-  }
-
-  const observer = new MutationObserver(callback)
-  observer.observe(campaignCookieBanner, {
-    attributes: true,
-    childList: true,
-    subtree: true
-  })
-}
